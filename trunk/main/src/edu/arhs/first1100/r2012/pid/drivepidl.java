d64e3e052594e7e0e72e0f3aeeee0a42b7e349cc
henk5066 <henk5066@4d8206bc-37c6-d931-850f-7c5646efc0ec>
2012-01-24 01:18:50 +0000

    this is a pid project, anything that involves a pid needs to use something in here or else the god of pids will come and murder you.

---
+++ trunk/main/src/edu/arhs/first1100/r2012/pid/drivepidl.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.arhs.first1100.r2012.pid;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+
+
+/**
+ *
+ * @author Connor Moroney
+ */
+public class drivepidl  {
+
+}
---
+++ trunk/main/src/edu/arhs/first1100/r2012/pid/drivepidr.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.arhs.first1100.r2012.pid;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+
+class DriveSource implements PIDSource
+{
+    public double pidGet()
+        {
+            return 0;
+        }
+}
+/**
+ *
+ * @author Connor Moroney
+ */
+public class drivepidr {
+
+}
