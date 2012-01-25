bcb28e0a1291b92c472186af7724ce8d054ce1b3
chickenbellyfinn <chickenbellyfinn@4d8206bc-37c6-d931-850f-7c5646efc0ec>
2012-01-25 23:16:42 +0000

    Cleaning up gyropid => GyroPid tree conflict, part 2

---
+++ trunk/main/src/edu/arhs/first1100/r2012/pid/GyroPid.java
@@ -0,0 +1,50 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package edu.arhs.first1100.r2012.pid;
+import edu.wpi.first.wpilibj.Gyro;
+import edu.wpi.first.wpilibj.Jaguar;
+import edu.wpi.first.wpilibj.PIDOutput;
+import edu.wpi.first.wpilibj.PIDSource;
+
+class GyroSource implements PIDSource
+{
+    Gyro source;
+    
+    public double pidGet()
+    {
+        return source.getAngle();
+    }
+   public GyroSource(Gyro g){
+       source = g;              
+   }
+}
+
+class GyroOutput implements PIDOutput
+{
+    Jaguar output;
+    
+    public void pidWrite(double o)
+    {
+        output.pidWrite(-o);
+    }
+    
+    public GyroOutput(Jaguar j){
+        output = j;        
+    }
+}
+/**
+ *
+ * @author Connor Moroney
+ */
+public class GyroPid extends edu.wpi.first.wpilibj.PIDController{
+    static private final double P = 0.005;
+    static private final double I = 0.0001;
+    static private final double D = 0.0;
+
+public GyroPid(Gyro source, Jaguar output){
+    super (P,I,D, new GyroSource(source), new GyroOutput(output));
+}
+}
