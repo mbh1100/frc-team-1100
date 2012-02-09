660646ee8dc2a282e89053e35ba16d463b3d0992
mbhxyz@gmail.com <mbhxyz@gmail.com@4d8206bc-37c6-d931-850f-7c5646efc0ec>
2012-02-09 03:09:35 +0000

    WingManExtreme

--- trunk/main/src/edu/arhs/first1100/oopctl/WingManExtreme.java
+++ trunk/main/src/edu/arhs/first1100/oopctl/WingManExtreme.java
@@ -20,11 +20,12 @@ public class WingManExtreme extends SystemBase {
     private Button start;
     private Button redbutton;
 
-    public WingManExtreme(int ch) {   
+    public WingManExtreme(int ch)
+    {
         channel = ch;
         js = new Joystick(ch);
-        xaxis = new JoystickAxis(js, Joystick.kDefaultXAxis);
-        yaxis = new JoystickAxis(js, Joystick.kDefaultYAxis);
+        xaxis = new JoystickAxis(js, 1);
+        yaxis = new JoystickAxis(js, 2);
         a = new Button(js, 1);
         b = new Button(js, 2);
         c = new Button(js, 3);
@@ -37,79 +38,91 @@ public class WingManExtreme extends SystemBase {
         redbutton = new Button(js, 10);
     }
 
-    public void bindYaxis(JoystickAxisHandler h) {
+    public void bindYaxis(JoystickAxisHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", Y Axis";
         h.setName(name);
         yaxis.bind(h);
     }
 
-    public void bindXaxis(JoystickAxisHandler h) {
+    public void bindXaxis(JoystickAxisHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", X Axis";
         h.setName(name);
         xaxis.bind(h);
     }
 
-    public void bindA(ButtonHandler h) {
+    public void bindA(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button A";
         h.setName(name);
         a.bind(h);
     }
 
-    public void bindB(ButtonHandler h) {
+    public void bindB(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button B";
         h.setName(name);
         b.bind(h);
     }
 
-    public void bindC(ButtonHandler h) {
+    public void bindC(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button C";
         h.setName(name);
         c.bind(h);
     }
 
-    public void bindX(ButtonHandler h) {
+    public void bindX(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button X";
         h.setName(name);
         x.bind(h);
     }
 
-    public void bindY(ButtonHandler h) {
+    public void bindY(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button Y";
         h.setName(name);
         y.bind(h);
     }
 
-    public void bindZ(ButtonHandler h) {
+    public void bindZ(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button Z";
         h.setName(name);
         z.bind(h);
     }
 
-    public void bindLefttrig(ButtonHandler h) {
+    public void bindLefttrig(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", left trigger";
         h.setName(name);
         lefttrig.bind(h);
     }
 
-    public void bindRighttrig(ButtonHandler h) {
+    public void bindRighttrig(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", right trigger";
         h.setName(name);
         righttrig.bind(h);
     }
 
-    public void bindStart(ButtonHandler h) {
+    public void bindStart(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", button Start";
         h.setName(name);
         start.bind(h);
     }
 
-    public void bindRedbutton(ButtonHandler h) {
+    public void bindRedbutton(ButtonHandler h)
+    {
         String name = "Wingman Extreme channel " + channel + ", red button";
         h.setName(name);
         redbutton.bind(h);
     }
-    
-    void tick()
+
+    public void tick()
     {
         a.update();
         b.update();
@@ -122,6 +135,6 @@ public class WingManExtreme extends SystemBase {
         righttrig.update();
         lefttrig.update();
         xaxis.update();
-        yaxis.update();                
+        yaxis.update();
     }
 }
