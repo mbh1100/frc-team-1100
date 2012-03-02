5a5e6fbddc6698ead7c81607bfaa8ed2205c1d6a
chickenbellyfinn <chickenbellyfinn@4d8206bc-37c6-d931-850f-7c5646efc0ec>
2012-03-02 04:07:26 +0000

    Tue night commit

---
+++ trunk/main/src/edu/arhs/first1100/r2012/manipulator/RampArm.java
@@ -0,0 +1,89 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.arhs.first1100.r2012.manipulator;
+
+import edu.arhs.first1100.util.SystemBase;
+import edu.wpi.first.wpilibj.DigitalInput;
+
+/**
+ *
+ * @author team1100
+ */
+public class RampArm extends SystemBase
+{
+    DigitalInput upperSwitch;
+    DigitalInput lowerSwitch;
+
+    private static final double kRampSpeed = 0.5;
+
+    private double rampSpeed;
+
+    private RampArm()
+    {
+        // check switches 10 times per second
+        super(100);
+        upperSwitch = new DigitalInput(1,1);
+        lowerSwitch = new DigitalInput(1,2);
+    }
+
+    public boolean isUpperLimit()
+    {
+        return !upperSwitch.get();
+    }
+
+    public boolean isLowerLimit()
+    {
+        return !lowerSwitch.get();
+    }
+
+    public void moveUp()
+    {
+        rampSpeed = kRampSpeed;
+    }
+
+    public void moveDown()
+    {
+        rampSpeed = -kRampSpeed;
+    }
+
+    public void dontMove()
+    {
+        rampSpeed = 0;
+    }
+
+    public void stop()
+    {
+        rampSpeed = 0;
+        super.stop();
+    }
+
+    public void tick()
+    {
+        if (rampSpeed > 0 && !isUpperLimit() ||
+            rampSpeed < 0 && !isLowerLimit())
+        {
+            ManipulatorSystem.getInstance().setRampArm(rampSpeed);
+        }
+        else
+        {
+            ManipulatorSystem.getInstance().setRampArm(0.0);
+        }
+    }
+
+    private static RampArm instance = null;
+
+    public static RampArm getInstance()
+    {
+        if(instance == null)
+        {
+            instance = new RampArm();
+            instance.start();
+        }
+
+        return instance;
+    }
+
+
+}
