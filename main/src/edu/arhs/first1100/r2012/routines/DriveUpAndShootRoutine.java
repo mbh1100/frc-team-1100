95a4ce7e1ccd5dde1b0e9abdf61d9530c9860f47
mbhxyz <mbhxyz@4d8206bc-37c6-d931-850f-7c5646efc0ec>
2012-03-28 00:44:14 +0000

    Post-Boston. Shoot from the key in autonomous.

--- main/src/edu/arhs/first1100/r2012/routines/DriveUpAndShootRoutine.java
+++ main/src/edu/arhs/first1100/r2012/routines/DriveUpAndShootRoutine.java
@@ -32,37 +32,52 @@ public class DriveUpAndShootRoutine extends Routine {
         System.out.println("");
         // TODO: Modify Drive to use encoders
         // Drive forward for a while
-        DriveSystem.getInstance().driveLeft(-0.3);
-        DriveSystem.getInstance().driveRight(-0.3);
-        //if(!turret.isEnable())turret.enable();
-        //turret.setSetpoint(0.0);
-        //move the lead screw up all the way
-        //ManipulatorSystem.getInstance().setLeadScrewTilt(-1.0);
-        // Spin up the shooter while we're driving
-        ManipulatorSystem.getInstance().setShooterSpeed(DriverStation.getInstance().getAnalogIn(AUTO_PWR_CHANNEL));
-        Timer.delay(3.1);
-        // stop and shoot
-        DriveSystem.getInstance().driveLeft(0.0);
-        DriveSystem.getInstance().driveRight(0.0);
-        ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kForward);
-        ManipulatorSystem.getInstance().setMainLiftBelt(1.0);
-        ManipulatorSystem.getInstance().setShooterFeedWheels(-1.0);
-        // 5 seconds should be long enough to get off a couple of shots
-        // Akshay : at some time we could make it wait until the limit switch has been pressed 2 times
-        Timer.delay(5.0);
-        ManipulatorSystem.getInstance().setShooterSpeed(0.0);
-        ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kOff);
-        ManipulatorSystem.getInstance().setMainLiftBelt(0.0);
-        ManipulatorSystem.getInstance().setShooterFeedWheels(0.0);
-        //if(turret.isEnable())turret.disable();
-        ManipulatorSystem.getInstance().setTurretRotationSpeedAuto(0.0);
-        // now back up for a few seconds.
-        DriveSystem.getInstance().driveLeft(0.3);
-        DriveSystem.getInstance().driveRight(0.3);
-        Timer.delay(5.0);
-        // and stop.
-        DriveSystem.getInstance().driveLeft(0.0);
-        DriveSystem.getInstance().driveRight(0.0);
+        if(DriverStation.getInstance().getDigitalIn(1)){
+            DriveSystem.getInstance().driveLeft(-0.3);
+            DriveSystem.getInstance().driveRight(-0.3);
+            //if(!turret.isEnable())turret.enable();
+            //turret.setSetpoint(0.0);
+            //move the lead screw up all the way
+            //ManipulatorSystem.getInstance().setLeadScrewTilt(-1.0);
+            // Spin up the shooter while we're driving
+            ManipulatorSystem.getInstance().setShooterSpeed(DriverStation.getInstance().getAnalogIn(AUTO_PWR_CHANNEL));
+            Timer.delay(3.1);
+            // stop and shoot
+            DriveSystem.getInstance().driveLeft(0.0);
+            DriveSystem.getInstance().driveRight(0.0);
+            ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kForward);
+            ManipulatorSystem.getInstance().setMainLiftBelt(1.0);
+            ManipulatorSystem.getInstance().setShooterFeedWheels(-1.0);
+            // 5 seconds should be long enough to get off a couple of shots
+            // Akshay : at some time we could make it wait until the limit switch has been pressed 2 times
+            Timer.delay(5.0);
+            ManipulatorSystem.getInstance().setShooterSpeed(0.0);
+            ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kOff);
+            ManipulatorSystem.getInstance().setMainLiftBelt(0.0);
+            ManipulatorSystem.getInstance().setShooterFeedWheels(0.0);
+            //if(turret.isEnable())turret.disable();
+            ManipulatorSystem.getInstance().setTurretRotationSpeedAuto(0.0);
+            // now back up for a few seconds.
+            DriveSystem.getInstance().driveLeft(0.3);
+            DriveSystem.getInstance().driveRight(0.3);
+            Timer.delay(5.0);
+            // and stop.
+            DriveSystem.getInstance().driveLeft(0.0);
+            DriveSystem.getInstance().driveRight(0.0);
+        }
+        else{
+            ManipulatorSystem.getInstance().setShooterSpeed(DriverStation.getInstance().getAnalogIn(2));
+            Timer.delay(4);
+            ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kForward);
+            ManipulatorSystem.getInstance().setMainLiftBelt(1.0);
+            ManipulatorSystem.getInstance().setShooterFeedWheels(-1.0);
+            Timer.delay(10);
+            ManipulatorSystem.getInstance().setShooterSpeed(0.0);
+            ManipulatorSystem.getInstance().setNeckBelt(Relay.Value.kOff);
+            ManipulatorSystem.getInstance().setMainLiftBelt(0.0);
+            ManipulatorSystem.getInstance().setShooterFeedWheels(0.0);
+            
+        }
         setDone();
 
     }
